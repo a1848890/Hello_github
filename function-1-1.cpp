1d62bb4ea2c2346c3273dacedec96d7dff27ffc7
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-03-16 09:28:34 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -12,8 +12,11 @@ int main(){
 void printer(int array[10][10]){
     for (int i=0; i<10; i++){
         for (int j=0; j<10; j++){
-            std::cout<<array[i][j]<<" ";
+            if (j == 9)
+                std::cout<<array[i][j]<<std::endl;
+            else{
+                std::cout << array[i][j]<<" ";
+            }
         }
-        std::cout << std::endl;
     }
 }
