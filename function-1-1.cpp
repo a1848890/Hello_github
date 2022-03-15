620aa9ac99f0c6ba11994bdd1eab5fc4ef67b9ea
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-03-16 09:17:10 +1030

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+void printer(int array[10][10]);
+
+int main(){
+    int array[10][10] = {{}};
+    printer(array);
+    return 0;
+}
+
+void printer(int array[10][10]){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            std::cout<<array[i][j]<<" ";
+        }
+        std::cout << std::endl;
+    }
+}
