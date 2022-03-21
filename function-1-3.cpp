661f05155936fb5780f2bc0d45d4ad593ead2286
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-03-21 13:05:49 +1030

    Update function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -13,7 +13,7 @@ int main(){
 */
 void cpyia(int old_array[],int new_array[],int length){
     
-    for (int i=0; i!=length; i++){
+    for (int i=0; i<length; i++){
         *(new_array+i) = *(old_array+i);
     }
 }
