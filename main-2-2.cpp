16a0e459eebbb4105c6a33b746b378f9a6cd3a4b
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-03-21 13:07:57 +1030

    Update main-2-2.cpp

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -9,6 +9,6 @@ int maximum_sum(int *nums,int length);
 int main(){
     int arr1[10] = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
     int maxi = maximum_sum(arr1,10);
-    cout << maxi << endl;
+    cout << maxi;
     return 0;
 }
