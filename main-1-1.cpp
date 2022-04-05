d08273dccc5e09ba8658b390d34034dfa94657c1
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-04-05 21:07:08 +1000

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -9,28 +9,28 @@ using namespace std;
 int main(){
     meerkat m1;
     m1.setName("Kitty");
-    string m1_name = m1.getName();
+    //string m1_name = m1.getName();
     m1.setAge(10);
-    int m1_age = m1.getAge();
+    //int m1_age = m1.getAge();
     
     meerkat m2;
     m2.setName("Kitkat");
-    string m2_name = m2.getName();
+   // string m2_name = m2.getName();
     m2.setAge(5);
-    int m2_age = m2.getAge();
+    //int m2_age = m2.getAge();
     
     meerkat m3;
     m3.setName("Owl");
-    string m3_name = m3.getName();
+    //string m3_name = m3.getName();
     m3.setAge(4);
-    int m3_age = m3.getAge();
+    //int m3_age = m3.getAge();
     
     
     meerkat m4;
     m4.setName("Kitty");
-    string m4_name = m4.getName();
+    //string m4_name = m4.getName();
     m4.setAge(10);
-    int m4_age = m4.getAge();
+    //int m4_age = m4.getAge();
     
     
     
