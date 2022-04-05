e3916b31118b003351aef5a06c7b0611966b1b85
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-04-05 20:26:29 +1000

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -12,11 +12,30 @@ int main(){
     string m1_name = m1.getName();
     m1.setAge(10);
     int m1_age = m1.getAge();
-
-    cout << "m1's age: " << m1_age << endl;
-    cout << "m1's name: " << m1_age << endl;
+    
+    meerkat m2;
+    m2.setName("Kitkat");
+    string m2_name = m2.getName();
+    m2.setAge(5);
+    int m2_age = m2.getAge();
+    
+    meerkat m3;
+    m3.setName("Owl");
+    string m3_name = m3.getName();
+    m3.setAge(4);
+    int m3_age = m3.getAge();
+    
+    
+    meerkat m4;
+    m4.setName("Kitty");
+    string m4_name = m4.getName();
+    m4.setAge(10);
+    int m4_age = m4.getAge();
+    
+    
+    
+    // cout << "m1's age: " << m1_age << endl;
+    //cout << "m1's name: " << m1_age << endl;
 
     return 0;
 }
-
-
