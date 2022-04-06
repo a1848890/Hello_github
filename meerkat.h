191cf4fd7de04ad64f4a2e17a13c811bdb3068c6
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-04-06 17:32:02 +0930

    Create meerkat.h

---
+++ meerkat.h
@@ -0,0 +1,38 @@
+#ifndef MEERKAT_H
+#define MEERKAT_H
+#include<string>
+using namespace std;
+
+class meerkat{
+private:
+    string _meerName;
+    int _meerAge;
+    
+public:
+     meerkat();         // no name or age is required to create a meerkat
+     void setName(string meerName);   // change the meerkat's name
+     string getName();
+     void setAge(int meerAge);        // change the meerkat's age
+     int getAge();
+};
+
+#endif
+
+// * * * * * * * * * * * * * * * // 
+
+/*
+
+int main(){
+    meerkat m1;
+    m1.setName("Kitty");
+    string m1_name = m1.getName();
+    m1.setAge(10);
+    int m1_age = m1.getAge();
+
+    cout << "m1's age: " << m1_age << endl;
+    cout << "m1's name: " << m1_age << endl;
+
+    return 0;
+}
+
+*/
