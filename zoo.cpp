b64d23260f8524b837e66dc06454ddb69d40104f
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-06-01 10:26:28 +0930

    Update zoo.cpp

--- zoo.cpp
+++ zoo.cpp
@@ -9,10 +9,21 @@ using namespace std;
 
 
 
+
 zoo :: zoo(string n, int cows, int lions){
     name = n;
     number_of_animals = cows + lions;
     animals = new animal* [cows+lions];
+    for (int i=0; i<cows; i++){
+        animal *cow = new vegie("Daisy", 100);
+        animals[i] = cow; // animals is an array of pointers to animal
+                          //*animals or animals[i] is pointers to animal
+    }
+
+    for (int i=0; i<lions; i++){
+        animal *lion = new hunter("Clarence", 50);
+        animals[cows+i] = lion;
+    }
 }
 
 string zoo :: get_name(){
