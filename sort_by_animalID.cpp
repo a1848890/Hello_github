ac0663b755b1639b17e1a607d763eeb3f3d08578
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-06-01 10:48:15 +0930

    Update sort_by_animalID.cpp

--- sort_by_animalID.cpp
+++ sort_by_animalID.cpp
@@ -25,7 +25,11 @@ void sort_by_animalID :: sort(animal **animals,int n){
 
     for (int j=n; j>0; j--){
         for (int i=0; i<n-1; i++){
-            if (animals[i]->get_animalID() > animals[i+1] -> get_animalID()) std :: swap(animals[i],  animals[i+1]);
+            if (animals[i]->get_animalID() > animals[i+1] -> get_animalID()){
+                animal* temp = animals[i];
+                animals[i] = animals[i+1];
+                animals[i+1] = temp;
+            }
         }
     }
 }
